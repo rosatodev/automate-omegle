ac258beb4728952bfdf0e48ea97897a57970984d
Rosato <rosato.dev@gmail.com>
2021-12-26 14:24:44 -0300

    agregue tk
    agregue TK

--- automate.py
+++ automate.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+from tkinter import *
+
 def launchBrowser():
 
     options.add_argument("--log-level=OFF")
@@ -35,12 +37,33 @@ def ingresos():
     while (cantMensajes < 0):
         cantMensajes = int(input("ERROR, el valor ingresado es inválido, intente de nuevo.\n\nIngrese cantidad de veces que quiera enviar el mensaje: "))
 
+    # leerArchivo = driver.find_element(By.ID, "MainContent_lbProgressFile2")
+
     return (mensajeOmegle, cantMensajes)
 
 options = webdriver.ChromeOptions()
 driver = launchBrowser()
 
+driver.execute_script("window.scrollTo(0, window.scrollY + 250)")
+
+root = Tk()
+
+l_mensajeOmegle = Label(root, text="Ingrese el mensaje a Spammear en Omegle")
+l_mensajeOmegle.pack()
+
+mensajeOmegle = Entry(root, width=30)
+mensajeOmegle.pack()
+
+l_cantMensajes = Label(root, text="Ingrese la cantidad de repeticiones del mensaje")
+l_cantMensajes.pack()
+
+cantMensajes = Entry(root, width=30)
+cantMensajes.pack()
+
+
 ingresos()
+
+root.mainloop()
 
 
 
